2072b5d67cd018825405910c64017c1bbb2c1e78
QA-62 Nina <ninochka_gold@mail.ru>
2023-10-23 13:07:27 +0400

    Initial

---
+++ src/test/java/ru/netology/PostmanEhoTest.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEhoTest {
+    @Test
+    void sholdReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
